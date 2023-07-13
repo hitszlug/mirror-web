4134821746c6d0c9a83c7b0df3fb6c8512e0a6ee
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2023-07-13 17:37:28 +0800

    feat: 加了镜像表格

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAutoResizer: typeof import('element-plus/es')['ElAutoResizer']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
     ElCol: typeof import('element-plus/es')['ElCol']
@@ -20,6 +21,7 @@ declare module 'vue' {
     ElMenuTem: typeof import('element-plus/es')['ElMenuTem']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElTableV2: typeof import('element-plus/es')['ElTableV2']
+    ElTag: typeof import('element-plus/es')['ElTag']
     MainPage: typeof import('./src/components/MainPage/MainPage.vue')['default']
     MirrorList: typeof import('./src/components/MainPage/components/MirrorList.vue')['default']
     NewsWidget: typeof import('./src/components/MainPage/components/NewsWidget.vue')['default']
--- src/components/MainPage/components/MirrorList.vue
+++ src/components/MainPage/components/MirrorList.vue
@@ -1,15 +1,76 @@
 <script lang="ts">
-import { ElCard, ElTableV2 } from 'element-plus/lib/components/index.js';
+import { ElCard, ElTableV2, ElAutoResizer, Column, ElTag } from 'element-plus/lib/components/index.js';
+import { h } from 'vue';
+// const columns = generateColumns();
+// const data = generateData(columns, 20);
+
+let id = 0;
+const dataGenerator = () => (
+    {
+        id: `archlinux-${id++}`,
+        name: "archlinux",
+        status: "success",
+        size: "100TB",
+        lastUpdate: "Today",
+        nextUpdate: "Tomorrow",
+    }
+)
+
+const columns: Column<any>[] = [
+    {
+        key: 'name',
+        title: 'Name',
+        dataKey: 'name',
+        width: 120,
+    },
+    {
+        key: 'status',
+        title: 'Status',
+        dataKey: 'status',
+        width: 100,
+        cellRenderer: ({ cellData: status }) => h(
+            ElTag,
+            { type: status, "disable-transitions": true },
+            status
+        )
+    },
+    {
+        key: 'size',
+        title: 'Size',
+        dataKey: 'size',
+        width: 120,
+    },
+    {
+        key: 'lastUpdate',
+        title: 'Last Update',
+        dataKey: 'lastUpdate',
+        width: 150,
+    },
+    {
+        key: 'nextUpdate',
+        title: 'Next Update',
+        dataKey: 'nextUpdate',
+        width: 150,
+    },
+
+]
 
 export default {
     components: {
-        ElCard, ElTableV2
+        ElCard, ElTableV2, ElAutoResizer, ElTag
+    },
+    data() {
+        return {
+            columns,
+            data: Array.from({ length: 40 }).map(dataGenerator)
+        }
     }
 }
 </script>
 
 
 <template>
+    <el-tag style="display: none;"></el-tag>
     <el-card class="box-card">
         <template #header>
             <div class="card-header">
@@ -17,8 +78,12 @@ export default {
                 <el-button class="button" text>Operation button</el-button>
             </div>
         </template>
-        <div>
-            <!-- <el-table-v2 :columns="columns" :data="data" :width="700" :height="400" fixed /> -->
+        <div class="table-container">
+            <el-auto-resizer>
+                <template #default="{ height, width }">
+                    <el-table-v2 :columns="columns" :data="data" :height="height" :width="width" fixed />
+                </template>
+            </el-auto-resizer>
         </div>
     </el-card>
 </template>
@@ -39,6 +104,11 @@ export default {
 }
 
 .box-card {
+    width: 100%;
+}
+
+.table-container {
+    height:900px;
     width: 100%;
 }
 </style>
